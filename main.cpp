d2949ede8caaf4c330f41e20ca0ad77630827889
AthamAkramov <At.akramov@newuu.uz>
2025-01-23 15:27:52 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,69 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main() {
-    int age;
 
-    cout<< "Enter your age: " <<endl;
-    cin >> age;
-    cout<< "Your age is: " <<age<<endl;
+    //problem1
 
-    return 0;
+    // string name;
+    // int age;
+    //
+    // cout << "Please, enter your name: ";
+    // cin >> name;
+    // cout << " How old are you? ";
+    // cin >> age;
+    //
+    // cout << " Great " << name << ", your age is " << age << endl << endl;
+
+
+   //  //problem2
+   //
+   // for ( int i=1; i<=5; ++i) {
+   //     cout << setw (5) << i;
+   //
+   //
+   //  }
+
+    //problem3
+
+    // int x,y;
+    // int temp=x;
+    // x=y;
+    // y=temp;
+    //
+    // cout << "Enter two integers: ";
+    // cin >> x >> y;
+    //
+    // cout << "Swapped values: " << "x= " << x << " y= " << y << endl;
+
+
+    // //problem4
+    //
+    // int n;
+    //
+    // cout << "Enter a number: ";
+    // cin >> n;
+    //
+    // cout << setw(10) << "Number" << setw(10) << "Square " <<endl;
+    // for (int i=1; i<=n; ++i) {
+    //     cout << setw(10) << i << setw(10) << i*i << endl;
+    // }
+
+
+    //problem5
+
+    // string text;
+    // cout << "Enter a text: ";
+    // cin >> text;
+    //
+    // int padding = (50 - text.length() ) /2;
+    //
+    //
+    //
+    // cout << setw(padding + text.length()) << text << setw (padding+text.length() ) << endl;  return 0;
+
+
+
 }
+
