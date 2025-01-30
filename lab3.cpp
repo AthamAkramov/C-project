d1106da03ae44f64677dde4ec4c884a143894468
AthamAkramov <At.akramov@newuu.uz>
2025-01-30 15:43:17 +0500

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //problem1
+    //     int a,b,c,d,e;
+    //cin>>a>>b>>c>>d>>e>>;
+    //     int sum=a+b+c+d+e;
+    //     cout << "Sum is "<< sum << endl;
+
+
+
+    //problem2
+
+    //     int id = 240123;
+    //     double gpa = 4.5;
+    //     int yearofstudy = 1;
+    //     int fee = 30000000;
+    //
+    //     cout<< "Student ID: " <<id<<endl<< "Student GPA: " <<gpa<<endl<<"Student Year of Study: " <<yearofstudy<<endl<< " Student Study fee: "<<fee<<endl;
+    //
+    //
+    //
+
+    //problem3
+
+    // short c;
+    // int F = c*9/5 + 32;
+    //
+    // cout << "Enter temperature in Celsius: ";
+    // cin >> c;
+    // cout << "Temperature in Fahrenheit: " << F << endl;
+    //
+
+    //problem4
+
+    // double cost;
+    //
+    // cout << "Enter item cost: ";
+    // cin >> cost;
+    //
+    // int item;
+    //
+    // cout << "Enter number of items: ";
+    // cin >> item;
+    //
+    // double totalcost = cost * item;
+    //
+    // cout << "Total cost is: $ " << totalcost << endl;
+
+
+
+    //problem5
+
+    // int x,y,z,t;
+    //
+    // cout << "Enter your family member ages: ";
+    // cin >>x>>y>>z>>t;
+    // float average= (x+y+z+t)/4;
+    //
+    // cout << "Average age is:" << average << endl;
+
+
+
+//problem6
+
+    // int length;
+    // int width;
+    //
+    // cout  << "Enter length: ";
+    // cin >> length;
+    // cout << "Enter width: ";
+    // cin >> width;
+    //
+    // int area= length*width;
+    // int perimeter = 2*length+2*width;
+    //
+    // cout << "The are of rectangle is " << area << endl;
+    // cout << "The perimeter of rectangle is " << perimeter << endl;
+
+
+    //problem7
+
+   //  int divident;
+   //  cout << "Enter a divident: ";
+   //  cin >> divident;
+   //
+   //  int divisor;
+   //  cout << "Enter a divisor: ";
+   //  cin >> divisor;
+   //
+   //
+   //  int quotient = (divident / divisor);
+   // int remainder = (divident % divisor);
+   //
+   //  cout << "The quotient of division is: " << quotient << endl << "The remainder is: " << remainder << endl;
+
+
+
+//problem8
+
+    // int amount;
+    // cout << "Amount in USD: ";
+    // cin >> amount;
+    //
+    // int exchangeRate;
+    // cout << "Exchange rate (1 USD to UZS) : ";
+    // cin >> exchangeRate;
+    //
+    // int totalAmount = amount * exchangeRate;
+    //
+    // cout << "Total equivalent amount is : " << totalAmount << endl;
+
+
+//problem9
+// int r;
+//
+//     cout << "Please enter a radius: ";
+//     cin >> r;
+//
+//     cout << "Surface area of circle with r= " <<r << " is " << 3.14 *r*r <<endl;
+
+
+    // int r;
+    // int h;
+    //
+    // cout << "Please enter a radius: ";
+    // cin >> r;
+    //
+    // cout << "Please enter a height: ";
+    // cin >> h;
+    //
+    // float volume = 3.14 * r * r * h;
+    //
+    // cout << " Volume of cylinder with r=" << r << " and h=" << h << " is " << volume << endl;
+
+
+
+
+    //problem11
+
+    // int distance;
+    // int time;
+    //
+    // cout << "Enter distance: ";
+    // cin >> distance;
+    //
+    // cout << "Enter time: ";
+    // cin >> time;
+    //
+    // int velocity = distance / time;
+    // int velocity2 = velocity * 0.62137;
+    //
+    // cout << "V= " << velocity << "km/h" << " and " << velocity2 << " miles/h " << endl;
+
+
+    //problem12
+
+    // int minutes;
+    //
+    // cout << "Enter minutes: ";
+    // cin >> minutes;
+    //
+    // int hours= minutes/60;
+    // int minutes2 = minutes%60;
+    //
+    // cout << " Total minutes: " << minutes  << endl << " Total time: " << hours << " hours " << " and " << minutes2 << " minutes " <<endl;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
