99efcef5534b1e32376a4b7a5273823ae71e67e8
AthamAkramov <At.akramov@newuu.uz>
2025-02-06 21:28:52 +0500

    Update lab3.cpp
    by Atham

--- lab3.cpp
+++ lab3.cpp
@@ -167,8 +167,140 @@ int main() {
     //
     // cout << " Total minutes: " << minutes  << endl << " Total time: " << hours << " hours " << " and " << minutes2 << " minutes " <<endl;
 
+    //problem13
+
+    // int price_per_unit;
+    // int cost_per_unit;
+    //
+    // cout << "Enter selling price per unit: ";
+    // cin >> price_per_unit;
+    //
+    // cout << "Enter variable cost price per unit: ";
+    // cin >> cost_per_unit;
+    //
+    // int fixed_cost = 10.000;
+    //
+    // float bep = fixed_cost/(price_per_unit - cost_per_unit);
+    //
+    // cout << "Break-even point is:" << bep << endl;
 
 
+
+    //problem14
+
+    // int basic_salary;
+    // int percentage;
+    //
+    // cout << "Please enter your salary: ";
+    // cin >> basic_salary;
+    // cout << "Please enter your percentage: ";
+    // cin >> percentage;
+    //
+    // double total_salary = basic_salary - (percentage * basic_salary)/100;
+    //
+    // cout << "Total salary is: " << total_salary << endl;
+
+
+
+    //problem15
+
+    // int original_price;
+    // int discount_percentage;
+    // int discount_amount;
+    // int final_price;
+    // cout << "Enter original price: ";
+    // cin >> original_price;
+    // cout << "Enter discount percentage: ";
+    // cin >> discount_percentage;
+    //
+    // discount_amount = (original_price * discount_percentage)/100;
+    //
+    // final_price = original_price - discount_amount;
+    //
+    // cout << "Discount amount: " << discount_amount << endl;
+    // cout << "Final price: " << final_price << endl;
+
+
+//problem16
+
+    // int gpa1,gpa2,gpa3,gpa4,gpa5;
+    // int hours1, hours2, hours3, hours4, hours5;
+    //
+    // cout<<"Grade points for courses: ";
+    // cin>>gpa1>>gpa2>>gpa3>>gpa4>>gpa5;
+    //
+    // cout<<"Credit hours for courses: ";
+    // cin>>hours1>>hours2>>hours3>>hours4>>hours5;
+    //
+    //
+    // float gpa =gpa1+gpa2+gpa3+gpa4+gpa5;
+    // int credit_hours = hours1+hours2+hours3+hours4+hours5;
+    // float average_gpa = (gpa*credit_hours)/credit_hours;
+    //
+    // cout<<"The average GPA is: " <<average_gpa<<endl;
+
+
+    // int initial_price;
+    // int final_price;
+    //
+    // cout << "Enter initial price: ";
+    // cin >> initial_price;
+    // cout << "Enter final price: ";
+    // cin >> final_price;
+    //
+    // float inflation_rate = ((final_price - initial_price) * 100)/initial_price;
+    //
+    // cout << "Inflation rate: " << inflation_rate << "%"<< endl;
+
+//problem18
+
+    // int org_tuition_fee, scholarship_rate;
+    //
+    // cout << "Please enter your tuition fee: ";
+    // cin >> org_tuition_fee;
+    //
+    // cout << "Please enter your scholarship rate: ";
+    // cin >> scholarship_rate;
+    //
+    // float scholarship_amount = (org_tuition_fee*scholarship_rate)/100;
+    //
+    // cout << "Scholarship Amount: " << scholarship_amount << endl;
+    //
+    // int fin_tuition_fee = org_tuition_fee - scholarship_amount;
+    //
+    // cout << "Final Tuition fee: " << fin_tuition_fee << endl;
+    //
+
+
+    // int P, R, T;
+    //
+    // cout << "Principal amount: ";
+    // cin >> P;
+    //
+    // cout << "Rate of interest: ";
+    // cin >> R;
+    //
+    // cout << "Time: ";
+    // cin >> T;
+    //
+    // int SI = (P * R * T)/100;
+    //
+    // cout << "The simple interest is: " << SI << endl;
+
+
+    // float P, R;
+    //
+    // cout << "Principal: ";
+    // cin >> P;
+    //
+    // cout << "Annual interest rate: ";
+    // cin >> R;
+    //
+    // float A = P * (1+(R /100)) * (1+(R /100));
+    //
+    // cout << "Compount interest after 2 years: " << A - P << endl;
+    //
+    // cout << "Total amount after 2 years: " << A << endl;
 
 
 
@@ -178,6 +310,14 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
 
 
 
